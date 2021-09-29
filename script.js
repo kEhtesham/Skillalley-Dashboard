73a3642e6ad2cc3a900f19a68d3d75268c7aeda6
kEhtesham <ehtesham.webosmotic@gmail.com>
2021-09-29 16:05:34 +0530

    Version 1

---
+++ script.js
@@ -0,0 +1,29 @@
+// Tab system
+
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+}
+
+function getsceFunction(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontents");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function getrangeFunction() {
+    var range = document.getElementById('changeRange');
+    console.log(range.value);
+}
